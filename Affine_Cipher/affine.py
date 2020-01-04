cdb1635800a0844365a151953d65979b13c3a0cf
Matt Robinson <mpr5377@g.rit.edu>
2020-01-04 17:04:40 -0500

    Update Documentation for Affine

--- Affine_Cipher/affine.py
+++ Affine_Cipher/affine.py
@@ -8,6 +8,15 @@ import math
 
 
 def encrypt(plaintext, k1, k2, mod):
+    """
+    Function: encrypt
+    Description: Use the affine cipher to encrypt the plaintext
+    :param plaintext: The text to be encrypted
+    :param k1: The first part of the key
+    :param k2: The second part of the key
+    :param mod: The length of the alphabet
+    :return: The encrypted message
+    """
     encrypted_text = ""
     plaintext = plaintext.split(" ")
     plaintext = "".join(plaintext)
@@ -20,6 +29,15 @@ def encrypt(plaintext, k1, k2, mod):
 
 
 def decrypt(ciphertext, k1, k2, mod):
+    """
+    Function: Decrypt
+    Description: Decrypt the ciphertext to plaintext
+    :param ciphertext: The text to be decrypted
+    :param k1: The first part of the key
+    :param k2: The second part of the key
+    :param mod: The length of the alphabet
+    :return: The decrypted message
+    """
     decrypted_text = ""
     if gcd(k1, mod) == 1 and 1 <= k2 <= mod and 1 <= k1 <= mod:
         for c in ciphertext.lower():
@@ -31,12 +49,26 @@ def decrypt(ciphertext, k1, k2, mod):
 
 
 def mult_inverse(a, m):
+    """
+    Function: mult_inverse
+    Description: Given a number a, and the modulo m, find the multiplicative inverse of a
+    :param a: The number to find the multiplicative inverse of
+    :param m: The modulo
+    :return: The multiplicative inverse of a
+    """
     for i in range(1,m):
         if a*i % m == 1:
             return i
 
 
 def gcd(a,m):
+    """
+    Function: gcd
+    Description: Finds the greatest common divisor of a and m
+    :param a: The smaller number
+    :param m: The larger number
+    :return: The greatest common divisor of a and m
+    """
     return math.gcd(a,m)
 
 
